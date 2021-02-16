805da2a9b186516b2b54556d66c41e0cb4446ebf
Dominik Gwóźdź <dwgwozdz@gmail.com>
2021-02-16 15:34:36 +0100

    merge 2 migrations in to one

--- migrations/Version20210216120543.php
+++ migrations/Version20210216120543.php
@@ -19,13 +19,13 @@ final class Version20210216120543 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE locker (id INT AUTO_INCREMENT NOT NULL, ip_address VARCHAR(16) NOT NULL, lock_until DATETIME NULL, attempt_count INT NOT NULL DEFAULT 0, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE newsletter (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, is_confirmed TINYINT(1) NOT NULL, signed_at DATETIME DEFAULT NULL, hash VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE locker');
+        $this->addSql('DROP TABLE newsletter');
     }
 }
