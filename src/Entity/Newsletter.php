9478f047865dcffafd5064489f428f012385c97d
Dominik Gwóźdź <dwgwozdz@gmail.com>
2021-02-16 07:58:47 +0100

    add newsletter Entity, Repository and migration script

---
+++ src/Entity/Newsletter.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsletterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NewsletterRepository::class)
+ */
+class Newsletter
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_confirmed;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $signed_at;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $hash;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getIsConfirmed(): ?bool
+    {
+        return $this->is_confirmed;
+    }
+
+    public function setIsConfirmed(bool $is_confirmed): self
+    {
+        $this->is_confirmed = $is_confirmed;
+
+        return $this;
+    }
+
+    public function getSignedAt(): ?\DateTimeInterface
+    {
+        return $this->signed_at;
+    }
+
+    public function setSignedAt(?\DateTimeInterface $signed_at): self
+    {
+        $this->signed_at = $signed_at;
+
+        return $this;
+    }
+
+    public function getHash(): ?string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+}
