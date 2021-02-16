6dd431be4ed1acabbe7cf9a2791eed59daae4885
Dominik Gwóźdź <dwgwozdz@gmail.com>
2021-02-16 10:18:36 +0100

    handle newsletter service in controller

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Service\NewsletterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewsletterController extends AbstractController
 {
+    public function __construct(protected NewsletterService $newsletterService){}
+
     #[Route('/newsletter', name: 'newsletter', methods: ['POST'])]
     public function index(): Response
     {
-        return $this->render('newsletter/index.html.twig', [
-            'controller_name' => 'NewsletterController',
-        ]);
+        return $this->json([$this->newsletterService->signIn($_POST['email'])]);
     }
 }
